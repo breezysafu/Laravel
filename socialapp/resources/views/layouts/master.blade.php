c084b783f40ad43ad5b8279167f34e9acab62be0
Sarfraj <breezy.safu@gmail.com>
2018-12-01 12:29:02 +0545

    14

--- socialapp/resources/views/layouts/master.blade.php
+++ socialapp/resources/views/layouts/master.blade.php
@@ -10,5 +10,16 @@
 <div class="container">
     @yield('content')
 </div>
+
+<script
+        src="https://code.jquery.com/jquery-3.3.1.js"
+        integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+        crossorigin="anonymous"></script>
+<script
+        src="https://code.jquery.com/jquery-migrate-3.0.1.js"
+        integrity="sha256-VvnF+Zgpd00LL73P2XULYXEn6ROvoFaa/vbfoiFlZZ4="
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="{{ URL::to('src/js/app.js') }}"></script>
 </body>
 </html>
